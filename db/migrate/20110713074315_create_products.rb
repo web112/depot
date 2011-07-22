8cd740e7eeb2b0ffa5dc7204aedfd99a75b546c0
web112 <web112@126.com>
2011-07-21 18:36:24 -0700

    Edited db/migrate/20110713074315_create_products.rb via GitHub

--- db/migrate/20110713074315_create_products.rb
+++ db/migrate/20110713074315_create_products.rb
@@ -1,12 +1,5 @@
 class CreateProducts < ActiveRecord::Migration
   def self.up
-    create_table :products do |t|
-      t.string :title
-      t.text :description
-      t.string :image_url
-      t.decimal :price
-
-      t.timestamps
     end
   end
 
