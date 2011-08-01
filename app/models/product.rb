033c159364b4826e1621841aeb92f6ae0af3e2b1
zoller <297512481@qq.com>
2011-08-01 16:27:04 +0800

    upload image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,43 +1,54 @@
 class Product < ActiveRecord::Base
-  
+  def uploaded_image= (file_field)
+    now = Time.now
+    @file_name =  now.strftime("%y%m%d%H%M%S") + '_' + sanitize_filename(file_field.original_filename)
+
+    self.image_url = "user_files/product_images/"+@file_name
+
+    if !file_field.original_filename.empty? and file_field.content_type.chomp =~ /gif|jpeg|png/
+      File.open("public/images/user_files/product_images/"+@file_name,"wb+") do |f|
+        f.write(file_field.read)
+      end
+    end
+  end
+
+  def sanitize_filename(filename)
+    filename = File.basename(filename.gsub("\\", "/")) # work-around for IE
+    filename.gsub!(/[^a-zA-Z0-9\.\-\+_]/,"_")
+    filename = "_#{filename}" if filename =~ /^\.+$/
+    filename = "unnamed" if filename.size == 0
+    filename
+  end
+
   default_scope :order => 'title'
-  
+
   has_many :line_items, :dependent => :destroy
   has_many :orders, :through => :line_items
-  
+
   belongs_to :shop
-  
-  
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
-  validates :image_url, :format => 
+  validates :image_url, :format =>
   {
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPC or PNG image.'
   }
-  
+
   before_destroy :ensure_not_referenced_by_any_line_item_in_orders
-  
+
   def ensure_not_referenced_by_any_line_item_in_orders
-    
+
     line_items.each do
-      |item|
+    |item|
       if item.order != nil
-        return false
+      return false
       end
     end
 
     return true;
-=begin
-    if line_items.count.zero?
-      return true
-    else
-      errors[:base] << "Line Item present"
-      return false
-    end
-=end
-    
   end
-  
+
 end
+
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,12 +1,38 @@
 class Shop < ActiveRecord::Base
+  def uploaded_image= (file_field)
+    now = Time.now
+    @file_name =  now.strftime("%y%m%d%H%M%S") + '_' + sanitize_filename(file_field.original_filename)
+
+    self.image_url = "user_files/shop_images/"+@file_name
+
+    if !file_field.original_filename.empty? and file_field.content_type.chomp =~ /gif|jpeg|png/
+      File.open("public/images/user_files/shop_images/"+@file_name,"wb+") do |f|
+        f.write(file_field.read)
+      end
+    end
+  end
+
+  def sanitize_filename(filename)
+    filename = File.basename(filename.gsub("\\", "/")) # work-around for IE
+    filename.gsub!(/[^a-zA-Z0-9\.\-\+_]/,"_")
+    filename = "_#{filename}" if filename =~ /^\.+$/
+    filename = "unnamed" if filename.size == 0
+    filename
+  end
+  
   has_many :users, :dependent => :destroy
   has_many :products, :dependent => :destroy
   has_many :orders
   
   
-  #validates :name, :email, :telephone, :image_url, :description, :presence => true
+  validates :name, :email, :telephone, :image_url, :description, :presence => true
   
   validates :name, :presence => true
+  validates :image_url, :format =>
+  {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPC or PNG image.'
+  }
 
 =begin
   validates :image_url, :format => 
